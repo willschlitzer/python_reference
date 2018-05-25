08c60382c5252b9e6a29d006344d7a8759c750d7
Will Schlitzer <schlitzer90@gmail.com>
2018-05-25 08:44:50 -0500

    Created string explanation

---
+++ basics/data_types.py
@@ -0,0 +1,9 @@
+"""An explanation of the three main data types used in Python"""
+
+# There are 3 main data types in Python: strings, integers, and floats
+
+# A string can include any character
+# A string's data is cannot be changed
+# Numbers do not have their value when they are in strings
+
+print(type('Hello'))
