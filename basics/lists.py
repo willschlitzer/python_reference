f1157399ba9c89c829cfe565e785a5b6bf1ed36b
Will Schlitzer <schlitzer90@gmail.com>
2018-05-25 13:45:57 -0400

    Began list explanation

--- basics/lists.py
+++ basics/lists.py
@@ -1,6 +1,14 @@
 """And explanation of lists in Python"""
 
 # A list is an ordered set of data
-# Every entry in a list is indexed, beginning at 0
+
 # Lists are defined with the square brackets: []
 # Every entry in a list is separated by a comma
+
+my_list = [1, 2, 3, 4, 5]
+
+# Every entry in a list is indexed, beginning at 0
+# The desired index of a list is indicated after the list name with square brackets
+print(my_list[3])
+
+# Data types in lists do not have to be uniform
