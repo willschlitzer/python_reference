36246d13f1e08b5938b67abb4dccb94fc5e98abc
Will Schlitzer <schlitzer90@gmail.com>
2018-05-25 08:31:45 -0500

    Added comments, docstrings, and print function for hello_world.py

---
+++ basics/hello_world.py
@@ -0,0 +1,5 @@
+"""An intro to Python 3, demonstrating printing Hello World"""
+# One of the first programs to write prints the phrase 'Hello World!'
+# In Python 3, print is a function
+# 'Hello World!' is a string (more on that later)
+print('Hello World!')
