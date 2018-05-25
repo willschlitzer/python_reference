1e92658cd68f8d8e420667470156f260634448d0
Will Schlitzer <schlitzer90@gmail.com>
2018-05-25 08:55:00 -0500

    Adding int and float explanations

--- basics/data_types.py
+++ basics/data_types.py
@@ -6,4 +6,24 @@
 # A string's data is cannot be changed
 # Numbers do not have their value when they are in strings
 
+# The type() function returns the type of data that was inputted to this
+# The print() function prints what type() returns
+# In this class, it is class 'str' for string
 print(type('Hello'))
+
+# An integer is a whole number
+# It does have a number value
+# When added, subtracted, or multiplied with another integer, the result is also an integer
+# Divison, even when resulting in a whole number, returns a float
+print(type(3))
+print(type(3+3))
+print(type(5-3))
+print(type(5*3))
+print(type(6/2))
+
+# A float is any number including decimals
+# When uses with an integer, the result is an float
+# Even when a whole number, the addition of a decimal point makes a number a float
+print(type(3.5))
+print(type(3.0))
+print(type(5 + 3.0))
