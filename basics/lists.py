e833e55cdec85f8c11f38c8e91096c357483adbd
Will Schlitzer <schlitzer90@gmail.com>
2018-05-25 13:33:20 -0400

    lists.py comments

--- basics/lists.py
+++ basics/lists.py
@@ -1,1 +1,6 @@
 """And explanation of lists in Python"""
+
+# A list is an ordered set of data
+# Every entry in a list is indexed, beginning at 0
+# Lists are defined with the square brackets: []
+# Every entry in a list is separated by a comma
