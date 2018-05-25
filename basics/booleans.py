ac7f27416d9ba0c0dc1cd0fff623344fe025c435
Will Schlitzer <schlitzer90@gmail.com>
2018-05-25 13:20:19 -0400

    Updated booleans.py for returned booleans from evaluated statements

--- basics/booleans.py
+++ basics/booleans.py
@@ -2,4 +2,15 @@
 
 # A Boolean is a True/False statement
 print(True)
+# Booleans can be used to determine if two values are equal
+# The double equal sign is a test for if two values are equivalent
 print(3 == 3)
+print('hello' == 'HELLO')
+print(3.0 == 3)
+
+# Booleans can be used to determine if something is in a string/list/dictionary:
+print('a' in 'antelope')
+print('x' in 'mongoose')
+
+
+
