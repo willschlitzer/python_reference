3b1382c4c3be05e2a058e6855e3d48d37c4741d9
Will Schlitzer <schlitzer90@gmail.com>
2018-05-31 05:37:24 -0500

    Updated lists.py

--- basics/lists.py
+++ basics/lists.py
@@ -10,5 +10,15 @@ my_list = [1, 2, 3, 4, 5]
 # Every entry in a list is indexed, beginning at 0
 # The desired index of a list is indicated after the list name with square brackets
 print(my_list[3])
+# Inidices also work in reverse
+# Returns the final item
+print(my_list[-1])
 
 # Data types in lists do not have to be uniform
+mix_list = [1, 'a', 3.0, True]
+
+# Data from lists can be sliced out with the indices
+# The indices of the start (inclusive) and end (non-inclusive) are given
+print(my_list[2:4])
+# An addition colon can be given with the steps (in this case 2) for slicing
+print(my_list[::2])
