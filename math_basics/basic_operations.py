6ff4b10966dc732d8334e6c734ac30f38ebee830
Will Schlitzer <schlitzer90@gmail.com>
2018-06-01 05:33:47 -0500

    Adding functions for basic_operations.py

--- math_basics/basic_operations.py
+++ math_basics/basic_operations.py
@@ -1,2 +1,32 @@
 """A demonstration of basic math operations in Python"""
 # Does not include the math module
+
+# Addition
+print('Addition')
+print(3+3)
+
+# Subtraction
+print('Subtraction')
+print(6-3)
+
+# Multiplication
+print('Multiplication')
+print(3*4)
+
+# Division
+print('Division')
+print(5/3.0)
+
+# Exponents
+print('Exponents')
+# The double asterisk (**) is used instead of the carat (^)
+print(3**2)
+
+# Order of operations is automatically implemented
+# Please Excuse My Dear Aunt Sarah
+# Parenthesis Exponents Multiplcation Division Addition Subtraction
+print('Order of operations')
+print('Without parenthesis')
+print(3+6/2**2)
+print('With parenthesis')
+print(((3+6)/2)**2)
