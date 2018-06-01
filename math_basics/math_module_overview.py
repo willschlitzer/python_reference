abcdb5dd50987c13a84fb7bf7a915d36a96650ab
Will Schlitzer <schlitzer90@gmail.com>
2018-06-01 05:37:23 -0500

    Initial commit math_module_overview.py

---
+++ math_basics/math_module_overview.py
@@ -0,0 +1,6 @@
+"""Demonstrates how to use the default math module in Python"""
+import math
+
+# Math module provides functions that can be applied as methods to variables and numbers
+print(math.sqrt(9))
+
