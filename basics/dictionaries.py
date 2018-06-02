846338e6cf7809388cd55631cadbb971c28472da
Will Schlitzer <schlitzer90@gmail.com>
2018-06-02 12:55:23 -0500

    Intro for dictionaries.py

--- basics/dictionaries.py
+++ basics/dictionaries.py
@@ -1,1 +1,7 @@
 """An explanation of dictionaries in Python"""
+
+# A dictionary is a data format that contains keys and values
+# keys must be immutable strings or tuples
+
+my_dict = {(3,2):1, 'a':2}
+print(my_dict)
