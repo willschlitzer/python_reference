c2d049ce5f7f87c5dbeab0cc8e17d35f43d3b512
Will Schlitzer <schlitzer90@gmail.com>
2018-06-05 20:06:29 -0500

    Updated classes.py, and wrote basics in for_loops.py

--- basics/classes.py
+++ basics/classes.py
@@ -1,1 +1,28 @@
 """An explanation of classes"""
+
+# Classes create an instance and store it in a variable
+# Data can be stored and changed within the variable
+
+
+class Friends:
+    """A class stores data and runs functions"""
+
+    def __init__(self, first, last, age):
+        """The init function runs upon the instance being called"""
+        self.age = age
+        self.first = str.title(first)
+        self.last = str.title(last)
+
+    def phone_number(self, number):
+        """Additional functions can be called in a class"""
+        # Subsequent variables can be assigned
+        self.number = number
+
+
+# Calls the init function in the class
+will = Friends("will", "schlitzer", 28)
+# Demonstrates the variable is stored from the class
+print(will.first)
+# Call a subsequent function to assign a new variable
+will.phone_number("508-742-8232")
+print(will.number)
--- basics/for_loops.py
+++ basics/for_loops.py
@@ -1,1 +1,6 @@
 """An explanation of for loops in Python"""
+
+# A for loop can be used to iterate through a generator or iterable
+
+for i in range(10):
+    print(i)
